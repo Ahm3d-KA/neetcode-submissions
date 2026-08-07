bde5b3b46f73fa16ee9786bbc09de0463515238a
Ahmed K <93289890+Ahm3d-KA@users.noreply.github.com>
2026-08-07 20:33:16 +0100

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l=0, r=0;
+        int maxp = 0;
+        while (r < prices.size()) {
+            if (prices[r] <= prices[l]) {
+                l = r;
+            }
+            maxp = max(maxp, prices[r] - prices[l]);
+            r++;
+        }
+        return maxp;
+    }
+};
